9ce1d48d5a36de7ea51ac33c68a2c8d415c5d1cb
Beshayr <beshayr37@gmail.com>
2020-12-15 00:38:54 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+const db = require("./models");
+const app = express();
+
+app.use(logger("dev"));
+
+// Use Express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
+
+//  Database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+// Server
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
