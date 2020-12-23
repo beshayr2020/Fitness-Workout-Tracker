f47fb2669940af626c8ee9e44849fdea33491e55
Beshayr <beshayr37@gmail.com>
2020-12-22 20:20:05 -0600

    editing text

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 
 
 //  Database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
